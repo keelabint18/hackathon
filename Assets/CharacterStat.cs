819f74783a8ccd53d6c3666beb53426ebb42a51b
t_era <36406139+sarzicet17@users.noreply.github.com>
2018-09-23 22:49:46 +0900

    Fix CharacterStat
    ファイル名とクラス名が異なっていたのを修正
    各オブジェクトに割り当てられるように修正

---
+++ Assets/CharacterStat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour {
+
+    //ジャンケン用ランダム値のシード
+    //   System.Random r = new System.Random(1000);
+
+    //まほうスライム
+    public int HP;
+    public int MagicA;
+    public int Attack;
+    public int Defend;
+
+    public bool attack_stat;
+    public bool defend_stat;
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
