9dcf2b2e10974e5abc8af5b34a27dcba417bb5d5
t_era <36406139+sarzicet17@users.noreply.github.com>
2018-09-24 10:29:16 +0900

    スライムの追加

--- Assets/BattleTurnStat.cs
+++ Assets/BattleTurnStat.cs
@@ -1,50 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BattleTurnStat : MonoBehaviour {
-    
-    //ジャンケン用乱数の設定
-    System.Random r = new System.Random();
+public class BattleTurnStat : MonoBehaviour
+{
 
     public bool MagicSlime_janken;
     public bool AttackCat_janken;
     public bool DefendHuman_janken;
-	// Use this for initialization
-	void Start () {
-	}
-
-    bool JudgeJanken_Character(string MyCharacter,string EnemyCharacter)
+    // Use this for initialization
+    void Start()
     {
+    }
+    //1 MyCharacter先行 0 EnemyCharacter 先行
+    //1 ぐー　2 チョキ 3 パー janken_status
+    int JudgeJanken_Character(string MyCharacter, string EnemyCharacter)
+    {
+        //ジャンケン用乱数の設定
+        int r = Random.Range(0, 2);
         if (GameObject.Find(MyCharacter).GetComponent<CharacterStat>().janken_status == GameObject.Find(EnemyCharacter).GetComponent<CharacterStat>().janken_status)
         {   //あいこのとき
-            return false;
+            return r;
         }
         else if ((GameObject.Find(MyCharacter).GetComponent<CharacterStat>().janken_status == 1) && (GameObject.Find(EnemyCharacter).GetComponent<CharacterStat>().janken_status == 3))
         {
             //自分がグー、相手がパー
-            return true;
+            return 1;
         }
         else if ((GameObject.Find(MyCharacter).GetComponent<CharacterStat>().janken_status == 2) && (GameObject.Find(EnemyCharacter).GetComponent<CharacterStat>().janken_status == 3))
         {
             //自分がチョキ、相手がパー
-            return true;
+            return 1;
         }
-        else {
-            return false;
+        else
+        {
+            return 0;
         }
     }
-
-    void SetCharacterJankenStat(int MyJanken,string MyCharacter,int EnemyJanken,string EnemyCharacter) {
+    void SetCharacterJankenStat(int MyJanken, string MyCharacter, int EnemyJanken, string EnemyCharacter)
+    {
         GameObject.Find(MyCharacter).GetComponent<CharacterStat>().janken_status = MyJanken;
         GameObject.Find(MyCharacter).GetComponent<CharacterStat>().janken_status = EnemyJanken;
     }
 
-    // Update is called once per frame
-    void Update () {
-        //1のときグー、2のときチョキ、3のときパー
 
-        SetCharacterJankenStat(r.Next(1, 4), "MagicSlime", r.Next(1, 4), "AttackCat");
-        JudgeJanken_Character("MagicSlime", "AttackCat");
-	}
+    //攻撃を開始する。ジャンケンの判定で勝ったほうが実行する。引数として現在経過しているターン数と、勝ったほうのキャラクター名を持ってくる。
+    int AttackStart(string Character,int CountTurn) {
+        //キャラクターの移動、ダメージを与える、受けるなどの処理
+        return CountTurn + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //1のときグー、2のときチョキ、3のときパー
+        //SetCharacterJankenStat(r, "MagicSlime", r, "AttackCat");
+        //JudgeJanken_Character("MagicSlime", "AttackCat");
+    }
 }
